2655b465d54ff0b8d98df8f24104cb8e44130404
zouhair <mizouhair4@gmail.com>
2025-01-10 19:34:49 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      minWidth: {
+        mymin: "400px",
+      },
+    },
+  },
+  plugins: [],
+};
